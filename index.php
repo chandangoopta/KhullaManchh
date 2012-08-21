180907246e5e93c202075521f4eea97796524880
Chandan Goopta <breaktech@linuxcandy.com>
2012-08-21 08:47:18 +0000

    updated

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php 
-
-//captcha session
 session_start();
 
 if( isset($_POST['submit'])) {
@@ -8,11 +6,9 @@ if( isset($_POST['submit'])) {
 		// Insert you code for processing the form here, e.g emailing the submission, entering it into a database. 
 $tweet_text = $_POST["tweet_text"];
 //print "Tweeting...\n";
-//Regex Match of any mentions made in tweet text. 
-if (preg_match ("/\@/i", $tweet_text)) {
-	print "you cannot mention anybody in ur tweet!"; }
+if (preg_match ("/\@/i", $tweet_text)) { print "you cannot mention anybody in tweeet!\n "; } 
 else {
-$result = post_tweet($tweet_text);}
+$result = post_tweet($tweet_text);} 
 //print "Response code: " . $result . "\n";
 if ($result=="200") echo "you are lovely. Your voice has been posted!";
 //      print "You just tweeted: " . $tweet_text . "\n";}       
@@ -27,50 +23,51 @@ if ($result=="200") echo "you are lovely. Your voice has been posted!";
 }
 ?>
 
+<!--	<form action="form.php" method="post">
+i--> 
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<link rel="stylesheet" href="exampl.css" type="text/css" media="screen" />
+		<script type="text/javascript" src="mtaTwitterStatuses.js"></script>
+		<title>mtaTwitterStatuses.js Example</title>
 </head>
 <body>
-<p><i>Post Your Tweet </i></p>
-<p> Share Something! </p>
- <form action="index.php" method="post"  style="height:100px;width:300px;" accept-charset="ISO-10646" >
-  <input type="tweet" name="tweet_text" />
+<table style="text-align: left; width 963px; height: 278px; "border="0"
+cellpadding="2" cellspacing="2">
+<tbody>
+<tr>
+<td style ="Vertical-align: top; width: 380px; height:243px;"><br>
+<img style="width: 348px; height: 128px;" alt="khulaa"
+src="khulaa.png"><br>
+<p> Post Your Voice </p> 
+<form action="form.php" method="post"    accept-charset="ISO-10646" >
+<textarea name="tweet_text" cols="30" rows="5">
+</textarea><br>
 </br></br>
 		<img src="CaptchaSecurityImages.php?width=100&height=40&characters=5" /><br />
-</br></br>		<label for="security_code">Insert Code: </label><input id="security_code" name="security_code" type="text" /><br />
+		<label for="security_code">Insert Code: </label><input id="security_code" name="security_code" type="text" /><br />
 		<input type="submit" name="submit" value="Submit" />
 	</form>
 </br></br></br></br>
-<!-- Here comes the twitter widget from twitter official. Must be replaced with light one. -->
-<script charset="utf-8" src="http://widgets.twimg.com/j/2/widget.js"></script>
-<script>
-new TWTR.Widget({
-  version: 2,
-  type: 'profile',
-  rpp: 20,
-  interval: 30000,
-  width: 250,
-  height: 300,
-  theme: {
-    shell: {
-      background: '#333333',
-      color: '#ffffff'
-    },
-    tweets: {
-      background: '#261126',
-      color: '#ffffff',
-      links: '#ebedf0'
-    }
-  },
-  features: {
-    scrollbar: true,
-    loop: false,
-    live: true,
-    behavior: 'all'
-  }
-}).render().setUser('KhulaaManch').start();
-</script>
+</div>
+</td>
+<td style="vertical-align: top; width: 428px; height: 243px;"><br>
+<!-- BEGIN TWITTER BADGE -->
+                <div id="mtaTwitter"></div>
+                <!-- OPTIONAL: -->
+                <script type="text/javascript">
+                        makkintosshu.twitterStatuses.elementId = 'mtaTwitter';
+                        makkintosshu.twitterStatuses.tweetCount = 20;
+                        makkintosshu.twitterStatuses.filterReplies = false;
+                        makkintosshu.twitterStatuses.showIcon = true;
+                </script>
+                <!-- REQUIRED: -->
+                <script type="text/javascript" src="http://www.twitter.com/statuses/user_timeline/khulaamanch.json?skip_user=true&callback=makkintosshu.twitterStatuses.twitterCallback"></script>
+                <!-- END TWITTER BADGE -->
+</td>
+</tr>
+</div>
 </body>
 </html>
 <?php
@@ -82,16 +79,26 @@ A sample code which uses OAuth to post a tweet!
 * @license GNU Public License
 */
 
+//$tweet_text = $argv[1];
+//$tweet_text="Test Tweets! 1 2 3 .... ";
+
+//$tweet_text = $_POST["tweet_text"];
+//print "Tweeting...\n";
+//$result = post_tweet($tweet_text);
+//print "Response code: " . $result . "\n";
+//if ($result=="200") echo "you are lovely. Your voice has been posted!";
+//      print "You just tweeted: " . $tweet_text . "\n";}       
+ //       else echo "We have tweetwet alot today! Try after few hours!";
 function post_tweet($tweet_text) {
    
   // Use Matt Harris' OAuth library to make the connection
   require_once('tmhoauth/tmhOAuth.php');
     
   $connection = new tmhOAuth(array(
-    'consumer_key' => '*********',
-    'consumer_secret' => '*********',
-    'user_token' => '***********',
-    'user_secret' => '********',
+    'consumer_key' => '****************',
+    'consumer_secret' => '***********************************',
+    'user_token' => '************************************',
+    'user_secret' => '',
   )); 
   
   // Make the API call
@@ -104,4 +111,3 @@ function post_tweet($tweet_text) {
 
 
 ?>
-
